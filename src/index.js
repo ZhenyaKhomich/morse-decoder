bc4d8417b87e19389054f329929da299cb4a9d7b
Zhenya Khomich <zhenyatsoy21061992@gmail.com>
2024-08-05 11:55:38 +0400

    finish

--- src/index.js
+++ src/index.js
@@ -38,7 +38,27 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let strFin = '';
+
+    for(let i = 0; i< expr.length; i+=10) {
+        let str10 = expr.slice(i, i + 10);
+            if(str10 == '**********') {
+                strFin += " ";
+                continue;
+        }
+    let a='';
+        for(let k = 0; k < str10.length; k+=2) {
+            let str2 = str10.slice(k, k + 2);
+        
+            if(str2 == '10') {
+                a+='.';
+            } else if(str2 == '11') {
+                a+='-';
+            }
+        } 
+        strFin+=MORSE_TABLE[a];
+    }
+    return strFin;
 }
 
 module.exports = {
